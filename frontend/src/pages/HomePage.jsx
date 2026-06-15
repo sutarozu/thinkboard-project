3c4c7cea5d7703116cc6e8bbd977cf7454b3f459
sutarozu <helmiputra265@gmail.com>
2026-06-15 16:15:08 +0700

    building createPage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar.jsx';
 import RateLimitedUI from '../components/RateLimitedUi.jsx';
-import axios from 'axios';
+import api from '../lib/axios.js';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard.jsx';
 
@@ -13,7 +13,7 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get('http://localhost:5001/api/notes');
+        const res = await api.get('/notes');
         console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
