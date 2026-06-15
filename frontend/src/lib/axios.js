c1598cfabcb525693e1981f0b7be0ade61b3959e
sutarozu <helmiputra265@gmail.com>
2026-06-15 17:37:18 +0700

    prepared for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// dalam production ga pake localhost makanya harus dibikin dinamic
+const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:5001/api' : '/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: BASE_URL,
 });
 
 export default api;
